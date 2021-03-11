812709d798ea1e039ca0b03273d27c7101f2c12e
Matikun <73756140+Matikun@users.noreply.github.com>
2021-03-11 01:34:27 -0300

    Update urls.jsx

--- src/helpers/urls.jsx
+++ src/helpers/urls.jsx
@@ -1,4 +1,4 @@
-const API_KEY = 'a89ca95107ae7e004e941df09e136aa5';
+const API_KEY = YOUR API KEY HERE;
 
 export const getWeatherUrl = ({ city, countryCode }) =>
 	`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${API_KEY}`;
